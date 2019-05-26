b35c77ff68a76959a600dcd8b1f83863b9388217
WqhForGitHub <353335238@qq.com>
2019-05-26 17:36:18 +0800

    移动设备端

--- src/view/main.vue
+++ src/view/main.vue
@@ -6,21 +6,39 @@
     </div>
     <a-divider></a-divider>
     <div class="serachField">
-      <a-input v-model="cityName" size="large" style="width:65%" placeholder="请输入要查询的城市名(支持中英文)">
-        <a-icon @click="serachCityName" slot="addonAfter" type="search" style="font-size:21px;cursor: pointer;"/>
-      </a-input>
-      <a-input size="large" style="width:45%;margin-left:35px;" :placeholder="time">
-        <a-icon slot="addonAfter" type="reload" style="font-size:21px;"/>
-      </a-input>
+      <a-row :gutter="40" justify="space-between">
+        <a-col :xs="20" :sm="14" :md="14" :lg="14" :xl="14">
+          <div class="cityInput">
+            <a-input  v-model="cityName" size="large" placeholder="输入城市名(支持中英文)">
+              <a-icon @click="serachCityName" slot="addonAfter" type="search" style="font-size:21px;cursor: pointer;"/>
+            </a-input>
+          </div>
+        </a-col>
+        <a-col :xs="20" :sm="10" :md="10" :lg="10" :xl="10">
+          <div class="updateTime">  
+            <a-input  size="large" :placeholder="time">
+              <a-icon slot="addonAfter" type="reload" style="font-size:21px;"/>
+            </a-input>
+          </div>
+        </a-col>
+      </a-row>
     </div>
     <a-divider>实时天气</a-divider>
     <nowCard :city="city" @update="timeUpdate"></nowCard>
     <a-divider>天气预报</a-divider>
-    <div style="display:flex;width:100%;justify-content: space-between;">
-      <todayCard :city="city" style="width:32%;"></todayCard>
-      <tomorrowCard :city="city" style="width:32%;"></tomorrowCard>
-      <afterTomoCard :city="city" style="width:32%;"></afterTomoCard>
-    </div>
+    <a-row>
+      <div style="display:flex;flex-wrap: wrap;width:100%;justify-content: space-between;">
+        <a-col :xs="24" :sm="23" :md="8" :lg="8" :xl="8">
+          <todayCard class="todayCard" :city="city"></todayCard>
+        </a-col>
+        <a-col :xs="24" :sm="23" :md="8" :lg="8" :xl="8">
+          <tomorrowCard class="tomorrowCard" :city="city"></tomorrowCard>
+        </a-col>
+        <a-col :xs="24" :sm="23" :md="8" :lg="8" :xl="8">
+          <afterTomoCard class="afterTomoCard" :city="city"></afterTomoCard>
+        </a-col>
+      </div> 
+    </a-row>
   </div>
 </template>
 
